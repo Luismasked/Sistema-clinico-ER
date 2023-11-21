1d2fe9e3b2ee6528defb1b884e3050cdbabd66d0
Luis Jose Mejia Ramos <ljmr9807@gmail.com>
2023-11-21 10:35:30 -0600

    Agregado entorno virtual, login e index

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,url_for,request,session
 app = Flask(__name__)
+
+app.secret_key = '_5#y2L"F4Q8zxec]/'
 
 @app.route('/')
 def index():
-    return render_template('index.html',title="Inicio",sesion="No tas logiado apa")
+    return render_template('index.html',title="Inicio")
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+@app.route('/ingresar',methods=["GET", "POST"])
+def ingresar():
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        session['email'] = email
+        return  render_template('prueba.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
